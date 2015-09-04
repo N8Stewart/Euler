ac2f5bf71387f9f9ab428120ba3324ac895a058d
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-03 22:29:37 -0400

    Complete solution

--- 011/problem.c
+++ 011/problem.c
@@ -1,4 +1,4 @@
-/*
+/* COMPLETE
  * What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
  * (Grid in separate text file to be loaded during runtime)
  */
